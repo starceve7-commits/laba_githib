5a000c56102d94a3ea921fbdd105f32ba2ef1dad
Egor Starcev <starceve7@gmail.com>
2025-12-07 21:17:37 +0500

    Add program skeleton and .gitignore

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
+// Чтение строк из файла в вектор
+vector <string> readFromFile(const string& filename) {
+    vector <string> lines;
+    // TODO: реализация чтения из файла
+    cout << "Функция readFromFile() пока не реализована." << endl;
+    return lines; // возвращаем пустой вектор для компиляции
+}
+
+// Вывод вектора строк на экран
+void printToScreen(const vector<string>& lines) {
+    // TODO: реализация вывода на экран
+    cout << "Функция printToScreen() пока не реализована." << endl;
+}
+
+// Запись вектора строк в файл
+void writeToFile(const vector<string>& lines, const string& filename) {
+    // TODO: реализация записи в файл
+    cout << "Функция writeToFile() пока не реализована." << endl;
+}
 
 int main() {
-	cout << "Hello! World";
-	return 0;
+    string inputFilename = "input.txt";
+    string outputFilename = "output.txt";
+
+    // Каркас будущей программы
+    vector<string> lines = readFromFile(inputFilename);
+    printToScreen(lines);
+    writeToFile(lines, outputFilename);
+
+    return 0;
 }
